0a44b263e88526dd4f210d87dcb8cd026a99772e
Rajatkumar jena <rajatkumarjena@Rajatkumars-MacBook-Air.local>
2024-11-07 20:19:39 +0530

    Added toastr alert

--- src/UI/src/app/subscription/store/subscription.effects.ts
+++ src/UI/src/app/subscription/store/subscription.effects.ts
@@ -1,45 +1,68 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, mergeMap, map, concatMap } from "rxjs/operators";
-import * as SubscriptionActions from './subscription.actions';
-import { from, of } from 'rxjs';
-import { SubscriptionService } from 'src/app/lib/openapi-generated/services';
-
-
+import * as SubscriptionActions from "./subscription.actions";
+import { from, of } from "rxjs";
+import { SubscriptionService } from "src/app/lib/openapi-generated/services";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class SubscriptionEffects {
   constructor(
     private actions$: Actions,
     private service: SubscriptionService,
-  ) { }
+    private toastr: ToastrService
+  ) {}
 
   loadSubscription$ = createEffect(() =>
     this.actions$.pipe(
       ofType(SubscriptionActions.loadSubscription),
       mergeMap(() => {
         return this.service.getSubscriptions().pipe(
-          map(data => SubscriptionActions.SubscriptionSuccess({ subscriptions: data })),
-          catchError(error => of(SubscriptionActions.SubscriptionError({ error: error.message })))
-        )
+          map((data) =>
+            SubscriptionActions.SubscriptionSuccess({ subscriptions: data })
+          ),
+          catchError((error) =>
+            of(SubscriptionActions.SubscriptionError({ error: error.message }))
+          )
+        );
       })
     )
-  )
-
+  );
 
   updateSubscription$ = createEffect(() =>
     this.actions$.pipe(
       ofType(SubscriptionActions.updateSubscription),
       mergeMap(({ subscriptions }) =>
-        from(this.service.updateSubscription({
-          body: subscriptions,
-          id: subscriptions.id
-        }))
-          .pipe(
-            map(() =>
-              SubscriptionActions.subscriptionUpdateSuccess({ subscriptionsId: subscriptions.id })),
-            catchError((error) => of(SubscriptionActions.SubscriptionError({ error: error.message })))
-          )
+        from(
+          this.service.updateSubscription({
+            body: subscriptions,
+            id: subscriptions.id,
+          })
+        ).pipe(
+          map(() => {
+            this.toastr.success(
+              "Subscription updated successfully",
+              "Success",
+              {
+                closeButton: true,
+                progressBar: true,
+              }
+            );
+            return SubscriptionActions.subscriptionUpdateSuccess({
+              subscriptionsId: subscriptions.id,
+            });
+          }),
+          catchError((error) => {
+            this.toastr.error("Fails to update subscription", "Error", {
+              closeButton: true,
+              progressBar: true,
+            });
+            return of(
+              SubscriptionActions.SubscriptionError({ error: error.message })
+            );
+          })
+        )
       )
     )
   );
@@ -49,14 +72,34 @@ export class SubscriptionEffects {
     this.actions$.pipe(
       ofType(SubscriptionActions.createSubscription),
       mergeMap(({ subscriptions }) =>
-        from(this.service.createSubscription({
-          body: subscriptions
-        }))
-          .pipe(
-            map((createdSubscriptionId) =>
-              SubscriptionActions.subscriptionCreateSuccess({ subscriptionsId: createdSubscriptionId })),
-            catchError((error) => of(SubscriptionActions.SubscriptionError({ error: error.message })))
-          )
+        from(
+          this.service.createSubscription({
+            body: subscriptions,
+          })
+        ).pipe(
+          map((createdSubscriptionId) => {
+            this.toastr.success(
+              "Subscription created successfully",
+              "Success",
+              {
+                closeButton: true,
+                progressBar: true,
+              }
+            );
+            return SubscriptionActions.subscriptionCreateSuccess({
+              subscriptionsId: createdSubscriptionId,
+            });
+          }),
+          catchError((error) => {
+            this.toastr.error("Failed to create subscription", "Error", {
+              closeButton: true,
+              progressBar: true,
+            });
+            return of(
+              SubscriptionActions.SubscriptionError({ error: error.message })
+            );
+          })
+        )
       )
     )
   );
@@ -67,8 +110,14 @@ export class SubscriptionEffects {
       ofType(SubscriptionActions.editSubscriptionById),
       mergeMap(({ id }) =>
         from(this.service.getSubscriptionDetails({ id })).pipe(
-          map((data) => SubscriptionActions.editSubscriptionByIdSuccess({ subscriptionsById: data })),
-          catchError((error) => of(SubscriptionActions.SubscriptionError({ error: error.message })))
+          map((data) =>
+            SubscriptionActions.editSubscriptionByIdSuccess({
+              subscriptionsById: data,
+            })
+          ),
+          catchError((error) =>
+            of(SubscriptionActions.SubscriptionError({ error: error.message }))
+          )
         )
       )
     )
